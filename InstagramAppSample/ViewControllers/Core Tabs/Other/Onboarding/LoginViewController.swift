62ad01768140ff5622dad5d07a8baf20c5e15f5c
Darya Kuliashova <kuldiand@gmail.com>
2021-02-05 17:29:38 +0300

    stubs for login added

--- InstagramAppSample/ViewControllers/Core Tabs/Other/Onboarding/LoginViewController.swift
+++ InstagramAppSample/ViewControllers/Core Tabs/Other/Onboarding/LoginViewController.swift
@@ -8,9 +8,78 @@
 import UIKit
 
 class LoginViewController: UIViewController {
+    
+    // MARK: - Properties
+    
+    private let usernameEmailField: UITextField = {
+        return UITextField()
+    }()
+    
+    private let passwordField: UITextField = {
+        let field = UITextField()
+        field.isSecureTextEntry = true
+        return field
+    }()
+    
+    private let loginButton: UIButton = {
+       return UIButton()
+    }()
+    
+    private let createAccountButton: UIButton = {
+        return UIButton()
+    }()
+    
+    private let privacyButton: UIButton = {
+        return UIButton()
+    }()
+    
+    private let termsButton: UIButton = {
+        return UIButton()
+    }()
+    
+    private let headerView: UIView = {
+       return UIView()
+    }()
 
+    // MARK: - VC lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        view.backgroundColor = .systemBackground
+        
+        addSubviews()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        //setup frames
+    }
+    
+    private func addSubviews() {
+        view.addSubview(usernameEmailField)
+        view.addSubview(passwordField)
+        view.addSubview(loginButton)
+        view.addSubview(createAccountButton)
+        view.addSubview(privacyButton)
+        view.addSubview(termsButton)
+        view.addSubview(headerView)
+    }
+    
+    @objc private func didTapLoginButton() {
+        
+    }
+    
+    @objc private func didTapCreateAccountButton() {
+        
+    }
+    
+    @objc private func didTapTermsButton() {
+        
+    }
+    
+    @objc private func didTapPrivacyButton() {
+        
     }
 }
